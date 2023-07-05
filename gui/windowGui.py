1592d6e7dfb3961989015f8a6f599747c406dbe8
Krzysztof Zielinski <zielinskikrzysztov@gmail.com>
2023-07-05 08:27:56 +0200

    #1 create structure files of app. Use GUI tkinter,database csv

---
+++ gui/windowGui.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from gui.communicatGui import *
+def main_gui():
+    root=Tk()
+    root.title("App hang man by Krzysztof Zielinski")
+    root.geometry("500x700")
+    root.mainloop()
